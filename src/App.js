6789ff326cccfa996b0af0f8fca6210a1e8dc760
abhishek ambure <abhisheknambure@gmail.com>
2021-12-14 23:00:09 +0530

    now committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App =() => {
+
+  const [result, setResult] = useState("");
+
+  const clickHandler = (event) => {
+    setResult(result.concat(event.target.value))
+  }
+
+  const clickClear = () => {
+    setResult("");
+  }
+
+  const displayresult = () => {
+    setResult(eval(result).toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='calci'>
+        <input type="text" placeholder='0' id='inputval' value={result}/>
+        <button onClick={clickHandler} value="9">9</button>
+        <button onClick={clickHandler} value="8">8</button>
+        <button onClick={clickHandler} value="7">7</button>
+        <button onClick={clickHandler} value="6">6</button>
+        <button onClick={clickHandler} value="5">5</button>
+        <button onClick={clickHandler} value="4">4</button>
+        <button onClick={clickHandler} value="3">3</button>
+        <button onClick={clickHandler} value="2">2</button>
+        <button onClick={clickHandler} value="1">1</button>
+        <button onClick={clickHandler} value="0">0</button>
+        <button onClick={clickHandler} value=".">.</button>
+        <button onClick={clickHandler} value="/">/</button>
+        <button onClick={clickHandler} value="*">*</button>
+        <button onClick={clickHandler} value="+">+</button>
+        <button onClick={clickHandler} value="-">-</button>
+        <button onClick={clickHandler} value="%">%</button>
+        <button onClick={clickClear}>Clear</button>
+        <button id='equals' onClick={displayresult}>=</button>
+      </div>
+  )
 }
 
 export default App;
